e759217b6191a614de0c7a9f51b66e3f286a5b0c
JamesIsIn <jamesisin@gmail.com>
2019-10-23 04:29:03 -0700

    Create TemplateBash.sh

---
+++ TemplateBash.sh
@@ -0,0 +1,40 @@
+#! /usr/bin/env bash 
+# Title   :  .sh 
+# Parent  :  .sh 
+# Author  :  JamesIsIn 20190918  Do something nice today.  
+
+# Purpose :   
+# 
+
+## 
+
+############### 
+#  Variables  # 
+
+
+
+# # debugging 
+# 
+# # 
+
+## 
+
+############### 
+#  Functions  # 
+
+
+
+function main() { 
+	# 
+} 
+
+## 
+
+########## 
+#  Main  # 
+
+main 
+
+exit $? 
+
+## 
