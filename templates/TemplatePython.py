29eaf99cb6565f3bf5e0bb2462d1c6c3f728808d
JamesIsIn <jamesisin@gmail.com>
2020-01-22 00:00:24 -0800

    Create TemplatePython.py

---
+++ templates/TemplatePython.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3 
+# Title   :  .py 
+# Parent  :  none 
+# Author  :  JamesIsIn 20190407 Do something nice today. 
+
+# Purpose :   
+# 
+
+## 
+
+
+
+## 
+
+############## 
+#  Includes  # 
+
+
+
+## 
+
+############### 
+#  Variables  # 
+
+
+
+## 
+
+############### 
+#  Functions  # 
+
+
+
+def main(): 
+	print() 
+
+## 
+
+########## 
+#  Main  # 
+
+main() 
+
+raise SystemExit(0) 
+
+## 
