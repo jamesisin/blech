70ee66e0e1f28da5892fdd01e25e4de005206884
JamesIsIn <jamesisin@gmail.com>
2024-01-23 11:59:39 -0800

    Update TemplatePython.py

--- templates/TemplatePython.py
+++ templates/TemplatePython.py
@@ -5,7 +5,6 @@
 
 # Purpose :   
 # 
-
 ## 
 
 
@@ -19,8 +18,8 @@
 
 ## 
 
-################# 
-#  Definitions  # 
+################## 
+#  Declarations  # 
 
 
 
@@ -39,7 +38,7 @@ def main():
 ########## 
 #  Main  # 
 
-if __name__ == '__main__': 
+if __name__ == "__main__":  
 	main() 
 
 raise SystemExit(0) 
