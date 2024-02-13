b4a3e353e4477801e916eef0c8da7ae3deb86e2f
JamesIsIn <jamesisin@gmail.com>
2024-02-13 08:31:52 -0800

    Update TemplatePython.py
    add utf-8 line and comment

--- templates/TemplatePython.py
+++ templates/TemplatePython.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3 
+# if running python2 and need utf-8 printing add:  # -*- coding: utf-8 -*- 
 # Title   :  .py 
 # Parent  :  none 
 # Author  :  JamesIsIn 20190407 Do something nice today. 
