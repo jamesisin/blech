1ae87597400521c13784129fb4b9b2874eeeacd4
JamesIsIn <jamesisin@gmail.com>
2024-12-01 15:06:09 -0800

    Update FileRename.sh
    add continue functionality and adjust output spacings

--- FileRename.sh
+++ FileRename.sh
@@ -13,21 +13,21 @@
 declare containingFolderPath="${1}" 
 declare filename 
 declare fileExtension 
-	fileExtension="." 
-declare StringSearch 
-	StringSearch=' _ ' 
-declare StringReplacement 
-	StringReplacement=' – ' 
+declare stringSought 
+declare stringReplacement 
 declare proceed 
 	proceed="n" 
 
 ## 
 
+# ToDo:  create continue function 
+# ToDo:  adjust spacings in output 
+
 ############### 
 #  Functions  # 
 
 function func_intro() { 
-	printf '%s\n' "" "Hello.  " "" 
+	printf '%s\n' "" "Hello.  " 
 	printf '%s\n' "Crtl-c at any time abandons any changes and exits the script.  " "" 
 } 
 
@@ -39,10 +39,10 @@ function func_getContainingFolder() {
 	containingFolderPath="${containingFolderPath//\\/}" 
 	if [ -d "${containingFolderPath}" ] ; then 
 		printf '%s\n' "I have confirmed this is a directory.  " 
-		printf '%s\n' "${containingFolderPath}" "" 
+		printf '%s\n' "${containingFolderPath}" 
 	else 
 		printf '%s\n' "I cannot confirm this is a directory.  " 
-		printf '%s\n' "${containingFolderPath}" "" 
+		printf '%s\n' "${containingFolderPath}" 
 	fi 
 } 
 
@@ -55,65 +55,76 @@ function func_getContainingFolderLoop() {
 
 function func_getFileExtension() { 
 	# ask the user for the file extension for the files for string swapping 
-	read -rep "Please provide the file extension for the files to be changed:  " -i "${fileExtension}" fileExtension 
+	read -rep "For the files to change, please provide the file extension (like .flac or blank for all files):  " -i "${fileExtension}" fileExtension 
 } 
 
-function func_getStringSearch() { 
+function func_getstringSought() { 
 	# ask the user for the string to be replaced 
-	printf '%s\n' "(Certain characters may require an escape.)  " 
 	oldIFS="${IFS}" && IFS='' 
-	read -rep "What is the string which will be replaced?  " -i "${StringSearch}" StringSearch 
-	printf '%s\n' "" "	\"${StringSearch}\"" 
+	read -rep "What is the string which will be replaced (must escape certain characters like \?)?  " -i "${stringSought}" stringSought 
+	printf '%s\n' "	\"${stringSought}\"" 
 	IFS="${oldIFS}" 
 } 
 
-function func_getStringReplacement() { 
+function func_getstringReplacement() { 
 	# ask the user for the string to insert as replacement 
 	oldIFS="${IFS}" && IFS='' 
-	read -rep "With what shall we replace that string?  " -i "${StringReplacement}" StringReplacement 
-	printf '%s\n' "	\"${StringReplacement}\"" 
+	read -rep "And what is the replacement string ( – )?  " -i "${stringReplacement}" stringReplacement 
+	printf '%s\n' "	\"${stringReplacement}\"" 
 	IFS="${oldIFS}" 
 } 
 
 function func_testReplacement() { 
 	# emulate name change for user evaluation 
-	cd "${containingFolderPath}" 
+	cd "${containingFolderPath}" || printf '%s\n' "Unable to cd:  ${containingFolderPath} "  return 1 
 	for filename in *"${fileExtension}" ; do 
-		printf '%b\n' "${filename}  →  " "${filename/${StringSearch}/${StringReplacement}}" "" 
+		if [[ "${filename}" =~ ${fileExtension} ]] && [[ "${filename}" =~ ${stringSought} ]] && [[ "${filename}" != "${filename/${stringSought}/${stringReplacement}}" ]] ; then # do not quote regex or it is a string 
+			printf '%b\n' "${filename} 	→ 	${filename/${stringSought}/${stringReplacement}}" 
+		fi 
 	done 
 } 
 
 function func_testReplacementLoop() { 
 	# loop through tests 
-	while [ ! "${proceed}" == y ] ; do # something 
+	while [[ ! "${proceed}" =~ [y|Y] ]] ; do # something 
 		func_getFileExtension 
-		func_getStringSearch 
-		func_getStringReplacement 
+		func_getstringSought 
+		func_getstringReplacement 
 		func_testReplacement 
 		# ask user if test was ok 
-		printf '%s\n' "	\"${StringSearch}\"  →  \"${StringReplacement}\"" 
+		printf '%s\n' "	\"${stringSought}\"  →  \"${stringReplacement}\"" 
 		read -rp "Shall we proceed with this change?  (y|N)  " -n1 proceed 
-		printf "\n" 
+		printf '\n' 
 	done 
 } 
 
 function func_performReplacement() { 
 	# currently this is the original script version of the replacement 
-	for filename in *"${fileExtension}" ; do 
-		if mv "$filename" "${filename/${StringSearch}/${StringReplacement}}"  ; then 
-			printf '%s\n' "	${filename} changed " "" 
-		else 
-			printf '%s\n' 
+	for filename in *"${fileExtension}" ; do #  ??  ToDo:  this can be made more robust; only mv if file extension matches  ??  
+		if [[ "${filename}" =~ ${fileExtension} ]] && [[ "${filename}" =~ ${stringSought} ]] && [[ "${filename}" != "${filename/${stringSought}/${stringReplacement}}" ]] ; then # do not quote regex or it is a string 
+			if mv "$filename" "${filename/${stringSought}/${stringReplacement}}"  ; then 
+				printf '%s\n' "	${filename} changed " 
+			else 
+				printf '%s\n' "Error:  ${filename} " 
+			fi 
 		fi 
 	done 
 } 
 
 function main() { 
 	func_intro 
-	func_getContainingFolderLoop
-	func_testReplacementLoop 
-	func_performReplacement 
-	return $? 
+	func_getContainingFolderLoop 
+	while true ; do 
+		func_testReplacementLoop 
+		func_performReplacement 
+		read -rep "Perform another chnange in this directory [Y|n]?  " -n1 
+		if [[ "${REPLY}" =~ [n|N] ]] ; then 
+			return 0 
+		else 
+			proceed=""  
+		fi 
+	done 
+	return ${?} 
 } 
 
 ## 
